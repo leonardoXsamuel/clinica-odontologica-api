dfc9cee21abc49b04c8d5118fa01fee7aae5cb26
Leonardo Samuel <leonardosmsilva2004@gmail.com>
2025-12-14 13:35:15 -0300

    refactor: inserindo RESPONSE ENTITY nos endpoints de Paciente Controller

--- src/main/java/odontologica/clinica/clinicaOdont/controller/PacienteController.java
+++ src/main/java/odontologica/clinica/clinicaOdont/controller/PacienteController.java
@@ -1,11 +1,14 @@
 package odontologica.clinica.clinicaOdont.controller;
 
-import odontologica.clinica.clinicaOdont.model.Paciente;
+import odontologica.clinica.clinicaOdont.dto.paciente.PacienteCreateDTO;
+import odontologica.clinica.clinicaOdont.dto.paciente.PacienteResponseDTO;
+import odontologica.clinica.clinicaOdont.dto.paciente.PacienteUpdateDTO;
 import odontologica.clinica.clinicaOdont.service.PacienteService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RequestMapping("/pacientes")
 @RestController
@@ -18,71 +21,72 @@ public class PacienteController {
     }
 
     @GetMapping("/teste")
-    public String testeDeRota(){
+    public String testeDeRota() {
         return "ROTA TESTE FUNCIONANDO PERFEITAMENTE";
     }
 
-    // GET
     @GetMapping
-    public List<Paciente> getPacientes(){
-        return pacienteService.getPacientes();
+    public ResponseEntity<List<PacienteResponseDTO>> getPacientes() {
+        return ResponseEntity.ok(pacienteService.getPacientes());
     }
 
     @GetMapping("{id}")
-    public Optional<Paciente> getPacientesById(@PathVariable Long id){
-        return pacienteService.getPacienteById(id);
+    public ResponseEntity<PacienteResponseDTO> getPacientesById(@PathVariable Long id) {
+        return ResponseEntity.ok(pacienteService.getPacienteById(id));
     }
 
     @GetMapping("cpf/{cpf}")
-    public Optional<Paciente> getPacienteByCPF(@PathVariable String cpf){
-        return pacienteService.getPacienteByCPF(cpf);
+    public ResponseEntity<PacienteResponseDTO> getPacienteByCPF(@PathVariable String cpf) {
+        return ResponseEntity.ok(pacienteService.getPacienteByCPF(cpf));
     }
 
     @GetMapping("nome/{nome}")
-    public Optional<Paciente> getPacienteByNome(@PathVariable String nome) throws Exception {
-        return pacienteService.getPacienteByNome(nome);
+    public ResponseEntity<PacienteResponseDTO> getPacienteByNome(@PathVariable String nome) throws Exception {
+        return ResponseEntity.ok(pacienteService.getPacienteByNome(nome));
     }
 
     @GetMapping("telefone/{telefone}")
-    public Optional<Paciente> getPacienteByTelefone(@PathVariable String telefone){
-        return pacienteService.getPacienteByTelefone(telefone);
+    public ResponseEntity<PacienteResponseDTO> getPacienteByTelefone(@PathVariable String telefone) {
+        return ResponseEntity.ok(pacienteService.getPacienteByTelefone(telefone));
     }
 
-    // POST
     @PostMapping
-    public Paciente createPaciente(@RequestBody Paciente paciente){
-        return pacienteService.createPaciente(paciente);
+    public ResponseEntity<PacienteResponseDTO> createPaciente(@RequestBody PacienteCreateDTO dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(pacienteService.createPaciente(dto));
     }
 
     @PostMapping("/lote")
-    public List<Paciente> createPacientes(@RequestBody List<Paciente> pacienteList){
-        return pacienteService.createPacientes(pacienteList);
+    public ResponseEntity<List<PacienteResponseDTO>> createPacientes(@RequestBody List<PacienteCreateDTO> pacienteList) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(pacienteService.createPacientes(pacienteList));
     }
 
-    // PUT
     @PutMapping("{id}")
-    public Paciente updatePacienteById(@PathVariable Long id, @RequestBody Paciente novoPaciente) throws Exception {
-        return pacienteService.updatePacienteById(id, novoPaciente);
+    public ResponseEntity<PacienteResponseDTO> updatePacienteById(@PathVariable Long id, @RequestBody PacienteUpdateDTO novoPaciente) {
+        PacienteResponseDTO dto = pacienteService.updatePacienteById(id, novoPaciente);
+        return ResponseEntity.ok(dto);
     }
 
     @PutMapping("/cpf/{cpf}")
-    public Paciente updatePacienteByCPF(@PathVariable String cpf, @RequestBody Paciente novoPaciente) throws Exception {
-        return pacienteService.updatePacienteByCPF(cpf, novoPaciente);
+    public ResponseEntity<PacienteResponseDTO> updatePacienteByCPF(@PathVariable String cpf, @RequestBody PacienteUpdateDTO novoPaciente) {
+        PacienteResponseDTO dto = pacienteService.updatePacienteByCPF(cpf, novoPaciente);
+        return ResponseEntity.ok(dto);
     }
 
     @PutMapping("/nome")
-    public Paciente updatePacienteByNome(@RequestParam String nome, @RequestBody Paciente novoPaciente) throws Exception {
-        return pacienteService.updatePacienteByNome(nome, novoPaciente);
+    public ResponseEntity<PacienteResponseDTO> updatePacienteByNome(@RequestParam String nome, @RequestBody PacienteUpdateDTO novoPaciente) {
+        PacienteResponseDTO dto = pacienteService.updatePacienteByNome(nome, novoPaciente);
+        return ResponseEntity.ok(dto);
     }
 
-    // DELETE
     @DeleteMapping("{id}")
-    public void deletePacienteById(@PathVariable Long id) throws Exception {
+    public ResponseEntity<Void> deletePacienteById(@PathVariable Long id) {
         pacienteService.deletePacienteById(id);
+        return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("/cpf/{cpf}")
-    public void deletePacienteByCPF(@PathVariable String cpf) throws Exception {
+    public ResponseEntity<Void> deletePacienteByCPF(@PathVariable String cpf) {
         pacienteService.deletePacienteByCPF(cpf);
+        return ResponseEntity.noContent().build();
     }
 }
