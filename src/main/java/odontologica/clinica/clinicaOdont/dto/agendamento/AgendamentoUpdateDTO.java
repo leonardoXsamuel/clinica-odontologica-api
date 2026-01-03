22ce78a53904e9283050b7bd4c6b371d52255192
Leonardo Samuel <leonardosmsilva2004@gmail.com>
2026-01-03 01:20:51 -0300

    refactor: removendo bean validations de JPA dos DTOs.

--- src/main/java/odontologica/clinica/clinicaOdont/dto/agendamento/AgendamentoUpdateDTO.java
+++ src/main/java/odontologica/clinica/clinicaOdont/dto/agendamento/AgendamentoUpdateDTO.java
@@ -1,9 +1,5 @@
 package odontologica.clinica.clinicaOdont.dto.agendamento;
 
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
 import odontologica.clinica.clinicaOdont.model.Dentista;
@@ -20,20 +16,13 @@ public record AgendamentoUpdateDTO(
         LocalDateTime dataHora,
 
         @NotNull
-        @Enumerated(EnumType.STRING)
         StatusAgendamento statusAgendamento,
 
-        @ManyToOne
         @NotNull
-        @JoinColumn(name = "dentista_id", nullable = false)
         Dentista dentista,
 
-        @ManyToOne
         @NotNull
-        @JoinColumn(name = "dentista_id", nullable = false)
         Paciente paciente,
 
-        @ManyToOne
         @NotNull
-        @JoinColumn(name = "dentista_id", nullable = false)
         Servico servico){ }
--- src/main/java/odontologica/clinica/clinicaOdont/dto/paciente/PacienteUpdateDTO.java
+++ src/main/java/odontologica/clinica/clinicaOdont/dto/paciente/PacienteUpdateDTO.java
@@ -1,6 +1,5 @@
 package odontologica.clinica.clinicaOdont.dto.paciente;
 
-import jakarta.persistence.Column;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
@@ -9,24 +8,19 @@ import org.hibernate.validator.constraints.NotBlank;
 import java.time.LocalDate;
 
 public record PacienteUpdateDTO (
-        @Column(nullable = false)
         @NotBlank(message = "o atributo NOME é obrigatório.")
         String nome,
 
         @NotBlank(message = "o atributo TELEFONE é obrigatório.")
-        @Column(nullable = false, unique = true)
         String telefone,
 
         @Email
         @NotBlank(message = "o atributo EMAIL é obrigatório.")
-        @Column(nullable = false, unique = true)
         String email,
 
-        @Column(nullable = false, unique = true)
         @NotBlank(message = "o atributo CPF é obrigatório.")
         String cpf,
 
         @Past
         @NotNull
-        @Column(nullable = false)
         LocalDate dataNascimento) { }
