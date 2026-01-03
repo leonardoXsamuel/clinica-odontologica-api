cba82e986d41d714bfc54ac5a2f0eca3b63ac484
Leonardo Samuel <leonardosmsilva2004@gmail.com>
2026-01-03 01:25:24 -0300

    refactor: cria classe que comporta o metodo atualizaCampos.

--- src/main/java/odontologica/clinica/clinicaOdont/service/validator/AtualizaCampos.java
+++ src/main/java/odontologica/clinica/clinicaOdont/service/validator/AtualizaCampos.java
@@ -1,2 +1,18 @@
-package odontologica.clinica.clinicaOdont.service.validator;public class AtualizaCampos {
+package odontologica.clinica.clinicaOdont.service.validator;
+
+import odontologica.clinica.clinicaOdont.dto.agendamento.AgendamentoUpdateDTO;
+import odontologica.clinica.clinicaOdont.model.Agendamento;
+import org.jetbrains.annotations.NotNull;
+
+public class AtualizaCampos {
+
+    public static void atualizarCampos(@NotNull AgendamentoUpdateDTO novoAgendamento, Agendamento antigoAgendamento) {
+        if (novoAgendamento.dataHora() != null) {
+            antigoAgendamento.setDataHora(novoAgendamento.dataHora());
+        }
+
+        if (novoAgendamento.statusAgendamento() != null) {
+            antigoAgendamento.setStatus(novoAgendamento.statusAgendamento());
+        }
+    }
 }
