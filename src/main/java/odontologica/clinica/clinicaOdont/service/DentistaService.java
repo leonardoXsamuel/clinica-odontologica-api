7de75b34b6a95a4482979cfae7b8890010e522c1
Leonardo Samuel <leonardosmsilva2004@gmail.com>
2026-01-11 14:53:42 -0300

    refactor: validando se dentista existe antes de criar na tabela.

--- src/main/java/odontologica/clinica/clinicaOdont/service/DentistaService.java
+++ src/main/java/odontologica/clinica/clinicaOdont/service/DentistaService.java
@@ -10,7 +10,9 @@ import odontologica.clinica.clinicaOdont.model.enums.Especialidade;
 import odontologica.clinica.clinicaOdont.repository.DentistaRepository;
 import odontologica.clinica.clinicaOdont.service.validator.DentistaValidator;
 import jakarta.validation.constraints.NotNull;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -70,18 +72,22 @@ public class DentistaService {
 
     public List<DentistaResponseDTO> getDentistaByEspecialidade(Especialidade especialidade) {
 
-            Optional<Dentista> listaDentistas = dentistaRepository.findByEspecialidade(especialidade);
+        Optional<Dentista> listaDentistas = dentistaRepository.findByEspecialidade(especialidade);
 
-            if (listaDentistas.isEmpty()) {
-                throw new ResourceNotFoundException("Nenhum dentista foi localizado.");
-            }
-
-            return listaDentistas.stream()
-                    .map(DentistaResponseDTO::new)
-                    .toList();
+        if (listaDentistas.isEmpty()) {
+            throw new ResourceNotFoundException("Nenhum dentista foi localizado.");
         }
 
+        return listaDentistas.stream()
+                .map(DentistaResponseDTO::new)
+                .toList();
+    }
+
     public DentistaResponseDTO createDentista(DentistaCreateDTO dto) {
+        if (dentistaRepository.findByCro(dto.cro()).isPresent()) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Dentista com este CRO já existe");
+        }
+
         Dentista dentista = new Dentista(dto);
         dentistaRepository.save(dentista);
         return new DentistaResponseDTO(dentista);
